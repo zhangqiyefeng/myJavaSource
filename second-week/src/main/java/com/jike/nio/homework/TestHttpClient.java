3f24b743db805e0155a0cf27834d6c2bea3284bd
zhangqi02 <zhang.qi02@iwhalecloud.com>
2021-07-04 18:03:49 +0800

    第一周作业提交

---
+++ second-week/src/main/java/com/jike/nio/homework/TestHttpClient.java
@@ -0,0 +1,49 @@
+/**
+ * All rights Reserved, Designed By www.iwhalecloud.com
+ * Copyright (c): 2021 www.iwhalecloud.com
+ * FileName: TestHttpClient
+ * Author:   yida
+ * Date:     2021/7/3 22:21
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+package com.jike.nio.homework;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+/**
+ * 功能描述: <br>
+ * 〈5.必做〉
+ *
+ * @author zq
+ * @date 2021/7/3
+ */
+public class TestHttpClient {
+
+    public static void main(String[] args) throws IOException {
+        //1.打开http连接
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://127.0.0.1:8801");
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+        if(response.getStatusLine().getStatusCode() == 200){
+            HttpEntity entity = response.getEntity();
+            // 使用工具类EntityUtils，从响应中取出实体表示的内容并转换成字符串
+            String string = EntityUtils.toString(entity, "utf-8");
+            System.out.println(string);
+        }
+        // 关闭资源
+        response.close();
+        httpClient.close();
+    }
+
+}
